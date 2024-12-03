2d94376c9157021c86258da8149d514ebfdbc953
Ofek Bar shalom <155369472+ofekbarshalom@users.noreply.github.com>
2024-12-03 17:50:13 +0200

    Delete Gym directory

--- Gym/src/gym/Exception/InstructorNotQualifiedException.java
+++
@@ -1,7 +0,0 @@
-package gym.Exception;
-
-public class InstructorNotQualifiedException extends Exception {
-    public InstructorNotQualifiedException(String message) {
-        super(message);
-    }
-}
