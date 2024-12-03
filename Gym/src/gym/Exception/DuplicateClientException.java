0c75e0c5d284c0f50005d14307b4a3dd534a09dd
Ofek Bar shalom <155369472+ofekbarshalom@users.noreply.github.com>
2024-12-03 18:57:46 +0200

    Delete Gym directory

--- Gym/src/gym/Exception/DuplicateClientException.java
+++
@@ -1,7 +0,0 @@
-package gym.Exception;
-
-public class DuplicateClientException extends Exception {
-    public DuplicateClientException(String message) {
-        super(message);
-    }
-}
--- Gym/src/gym/Gender.java
+++
@@ -1,6 +0,0 @@
-package gym;
-
-public enum Gender {
-    Male,
-    Female
-}
